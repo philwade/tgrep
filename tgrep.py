7cb49542497731a7f3c7987d91d6634c53c43eef
phil <>
2011-02-14 16:56:48 +0000

    branch:      reddit_log Get partial and complete time matching working nicely

--- tgrep.py
+++ tgrep.py
@@ -3,24 +3,33 @@ import re
 class lineDate:
     def __init__(self, date_line):
         self.line = date_line
-        matches = re.match('^([A-Za-z]{3})\s+([0-9]{1,2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})', self.line)
-        self.month = matches.group(1)
-        self.day = matches.group(2)
-        self.hour = matches.group(3)
-        self.minute = matches.group(4)
-        self.second = matches.group(5)
+        self.hour = None
+        self.minute = None
+        self.second = None
+        try:
+            matches = re.search('(?P<hour>[0-9]{2}):?', self.line)
+            self.hour = matches.group('hour')
+            matches = re.search('[0-9]{2}:(?P<minute>[0-9]{2}):?', self.line)
+            self.minute = matches.group('minute')
+            matches = re.search('[0-9]{2}:[0-9]{2}:(?P<second>[0-9]{2})', self.line)
+            self.second = matches.group('second')
+        except AttributeError:
+            if self.hour == None:
+                raise TypeError("Not a parsable date")
 
 if __name__ == "__main__":
-    arg1 = sys.argv[1]
-    arg2 = sys.argv[2]
+    try:
+        arg1 = sys.argv[1]
+        arg2 = sys.argv[2]
+    except IndexError:
+        pass
 
+    filename = arg1
     file = open(filename, "r")
 
     for line in file:
         print line
         ld = lineDate(line)
-        print ld.month
-        print ld.day
         print ld.hour
         print ld.minute
         print ld.second
