f7e57038eeaa44e5c5efc76492fbdd4d28feb768
Phil Wade <phil@philwade.org>
2011-05-26 13:33:17 -0400

    branch:      reddit_log Actually get regular expression matching working

--- tgrep.c
+++ tgrep.c
@@ -10,10 +10,29 @@ typedef struct line_date {
 int main(void)
 {
     lineDate date;
-    char newTime[] = "10:10:10";
+    char *pattern = "10:10:10111(1111)";
+    regex_t re;
+    regmatch_t matches[2];
 
+    if(regcomp(&re, pattern, REG_EXTENDED) != 0)
+    {
+        printf("Failed to compile regex");
+        return 1;
+    }
+
+    char *newTime = "10:10:101111111";
+
+    if(regexec(&re, newTime, 2, matches, 0) != 0)
+    {
+        printf("Failed to match regex");
+        return 1;
+    }
+    else
+    {
+        printf("%.*s", (int)(matches[1].rm_eo - matches[1].rm_so), &newTime[matches[1].rm_so]);
+    }
     buildLineDate(&date, newTime);
-    printf("%i \n", date.hour);
+    //printf("%i \n", date.hour);
     return 0;
 }
 
