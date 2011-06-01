66764dc12d054b19d31ab02bc079cabdb973aee0
Phil Wade <phil@philwade.org>
2011-06-01 15:33:00 -0400

    branch:      reddit_log Restructure things all over again as my understanding or malloc and C in general increases. Start forming a better structure for parsing dates and arguments

--- tgrep.c
+++ tgrep.c
@@ -10,19 +10,43 @@ typedef struct lineDate {
     struct lineDate* secondDate;
 } lineDate;
 
-int main(int argc, char* argv[])
+void buildLineDate(lineDate* myDate, char* inputTimeString)
 {
-    lineDate date;
+    myDate = (lineDate*) malloc(sizeof(lineDate));
+    int secondTimeOffset;
+    char* firstDate;
+    char* secondDate;
+
+    if(isRange(inputTimeString, &secondTimeOffset) != 0)
+    {
+        firstDate = (char*) malloc(strlen(inputTimeString) - secondTimeOffset);
+        secondDate = (char*) malloc(secondTimeOffset);
+        strcpy(firstDate, inputTimeString+secondTimeOffset);
+        strncpy(secondDate, inputTimeString, --secondTimeOffset);
+        populateLineDate(myDate, firstDate);
+        myDate->secondDate = (lineDate*) malloc(sizeof(lineDate));
+        populateLineDate(myDate->secondDate, secondDate);
+        printf("first: %s, second %s\n", firstDate, secondDate);
+        free(secondDate);
+    }
+    else
+    {
+        firstDate = (char*) malloc(strlen(inputTimeString));
+        strcpy(firstDate, inputTimeString);
+        populateLineDate(myDate, firstDate);
+        printf("first: %s\n", firstDate);
+    }
+    free(firstDate);
+}
+
+//only pass in single time as timeString
+int populateLineDate(lineDate* time, char* timeString)
+{
+    time->hour = 0;
     char *pattern = "([0-9]{1,2}):([0-9]{2}):([0-9]{2})|([0-9]{1,2}):([0-9]{2})";
     regex_t re;
     regmatch_t matches[4];
     int rangeOffset;
-
-    if(argc == 1)
-    {
-        printf("I need at least a timestamp to look for\n");
-        return 1;
-    }
 
     if(regcomp(&re, pattern, REG_EXTENDED) != 0)
     {
@@ -45,39 +69,7 @@ int main(int argc, char* argv[])
             printf("%.*s\n", (int)(matches[i].rm_eo - matches[i].rm_so), &newTime[matches[i].rm_so]);
         }
     }
-    buildLineDate(&date, argv[1]);
-    //printf("%i \n", date.hour);
     return 0;
-}
-
-int buildLineDate(lineDate* time, char* inputTimeString)
-{
-    int secondTimeOffset;
-    char* firstDate;
-    char* secondDate;
-
-    if(isRange(inputTimeString, &secondTimeOffset) != 0)
-    {
-        firstDate = (char*) malloc(strlen(inputTimeString) - secondTimeOffset);
-        secondDate = (char*) malloc(secondTimeOffset);
-        strcpy(firstDate, inputTimeString+secondTimeOffset);
-        strncpy(secondDate, inputTimeString, --secondTimeOffset);
-        printf("first: %s, second %s\n", firstDate, secondDate);
-    }
-    else
-    {
-        //make sure we're a date at all
-    }
-    time;
-    free(firstDate);
-    free(secondDate);
-    return 0;
-}
-
-lineDate parseDate(char* timeString)
-{
-    lineDate myDate;
-    return myDate;
 }
 
 int isRange(char* inputTime, int* offset)
@@ -94,3 +86,39 @@ int isRange(char* inputTime, int* offset)
 
     return 0;
 }
+
+int main(int argc, char* argv[])
+{
+    lineDate* date;
+    date = (lineDate*) malloc(sizeof(lineDate));
+
+    char *test = "10";
+    int test2 = atoi(test);
+        printf("%i" , test2);
+
+    if(argc == 1)
+    {
+        printf("I need at least a timestamp to look for\n");
+        return 1;
+    }
+
+    buildLineDate(date, argv[1]);
+    if(date->hour == -1 && argc > 2)
+    {
+        buildLineDate(date, argv[2]);
+        if(date->hour == -1)
+        {
+            printf("Bad timestamp or no timestamp given.\n");
+            return 1;
+        }
+    }
+    else
+    {
+        printf("I need at least a timestamp to look for\n");
+        return 1;
+    }
+    //printf("%i \n", date.hour);
+    return 0;
+}
+
+
